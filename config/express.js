da004dc57856dfdaf6ab1556357144fd924bf8f6
Gustavo Cortez <cmgustavo83@gmail.com>
2014-04-15 16:45:26 -0300

    Added support for http request methods.

--- config/express.js
+++ config/express.js
@@ -37,6 +37,13 @@ module.exports = function(app, historicSync, peerSync) {
   app.use(express.methodOverride());
   app.use(express.compress());
 
+  app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+  });
+
   if (config.publicPath) {
     var staticPath = path.normalize(config.rootPath + '/../' + config.publicPath);
 
