fe2f6ec6c48b74dd72709f52299eb86e06208e75
Manuel Araoz <manuelaraoz@gmail.com>
2014-07-31 16:07:14 -0300

    add example of websocket API use

--- app/controllers/socket.js
+++ app/controllers/socket.js
@@ -3,12 +3,19 @@
 // server-side socket behaviour
 var ios = null; // io is already taken in express
 var util = require('bitcore').util;
+var mdb = require('../../lib/MessageDb').default();
 
 module.exports.init = function(io_ext) {
   ios = io_ext;
   ios.sockets.on('connection', function(socket) {
     socket.on('subscribe', function(topic) {
+      console.log('subscribe ' + topic);
       socket.join(topic);
+    });
+    socket.on('message', function(m) {
+      mdb.addMessage(m.payload, m.from, m.to, function(err) {
+        if (err) throw err; // TODO: handle
+      });
     });
   });
 };
@@ -54,9 +61,12 @@ module.exports.broadcastSyncInfo = function(historicSync) {
 };
 
 module.exports.broadcastMessage = function(from, to, ts, message) {
-  console.log('sending socket: %s, %s, %s, %s', from, to, ts, JSON.stringify(message));
   if (ios) {
-    ios.sockets.in(to).emit(from + '-' + ts, message);
+    console.log('sending message '+to);
+    ios.sockets.in(to).emit(from, {
+      payload: message,
+      ts: ts
+    });
   }
 
 }
