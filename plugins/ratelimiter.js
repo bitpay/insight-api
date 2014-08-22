f76d3ba61bac52b922801f8e30caa632cb47a108
Manuel Araoz <manuelaraoz@gmail.com>
2014-08-22 15:05:54 -0400

    fix package and increase limits

--- plugins/ratelimiter.js
+++ plugins/ratelimiter.js
@@ -9,8 +9,8 @@ module.exports.init = function(app, config) {
   logger.info('Using ratelimiter plugin');
 
   config = config || {};
-  config.whitelistRPH = config.whitelistRPH || 50000;
-  config.normalRPH = config.normalRPH || 1000;
+  config.whitelistRPH = config.whitelistRPH || 500000;
+  config.normalRPH = config.normalRPH || 10000;
   config.blacklistRPH = config.blacklistRPH || 0;
 
   app.use(limiter({
