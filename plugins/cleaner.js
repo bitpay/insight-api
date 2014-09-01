1ce26a9a6deaf63ccb259d33322290849cd5a46b
Manuel Araoz <manuelaraoz@gmail.com>
2014-09-01 14:09:58 -0300

    cleaner plugin: dependencies

--- plugins/cleaner.js
+++ plugins/cleaner.js
@@ -1,7 +1,7 @@
-var microtime = require('microtime');
 var mdb = require('../lib/MessageDb').default();
 var logger = require('../lib/logger').logger;
 var preconditions = require('preconditions').singleton();
+var cron = require('cron');
 
 module.exports.init = function(config) {
   logger.info('Using cleaner plugin');
