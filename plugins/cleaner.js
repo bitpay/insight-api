349972a047eea38367b6f3bb8a20e8dabe647001
Manuel Araoz <manuelaraoz@gmail.com>
2014-09-01 14:34:20 -0300

    cleaner plugin: basic wires

--- plugins/cleaner.js
+++ plugins/cleaner.js
@@ -2,8 +2,20 @@ var mdb = require('../lib/MessageDb').default();
 var logger = require('../lib/logger').logger;
 var preconditions = require('preconditions').singleton();
 var cron = require('cron');
+var CronJob = cron.CronJob;
 
+
+var doClean = function() {
+  
+}
+  
 module.exports.init = function(config) {
   logger.info('Using cleaner plugin');
+  logger.info(config);
+  var job = new CronJob({
+    cronTime: config.cronTime || '0 * * * *',
+    onTick: doClean,
+    start: true
+  });
+  doClean();
 };
-
