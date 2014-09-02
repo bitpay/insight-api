28da92be37cb7057aa6f80233b83c282595e15e0
Manuel Araoz <manuelaraoz@gmail.com>
2014-09-02 11:08:12 -0300

    monitor plugin: add plugin main file

---
+++ plugins/monitor.js
@@ -0,0 +1,8 @@
+var mdb = require('../lib/MessageDb').default();
+var logger = require('../lib/logger').logger;
+var preconditions = require('preconditions').singleton();
+var microtime = require('microtime');
+
+
+module.exports.init = function(config) {
+};
