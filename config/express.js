86be9000375f333c5353dfee39e042c883e7a04b
Matias Alejo Garcia <ematiu@gmail.com>
2014-11-10 23:16:58 -0300

    remove duplicate headers definition

--- config/express.js
+++ config/express.js
@@ -36,11 +36,6 @@ module.exports = function(app, historicSync, peerSync) {
 
   if (config.enableEmailstore) {
     var allowCopayCrossDomain = function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-
-
       if ('OPTIONS' == req.method) {
         res.send(200);
         res.end();
