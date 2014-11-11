c958b4430fe2b161eaaec5582dd276d69ec48205
Matias Alejo Garcia <ematiu@gmail.com>
2014-11-10 23:16:58 -0300

    remove duplicate headers definition

--- config/headers.js
+++ config/headers.js
@@ -6,8 +6,8 @@ module.exports = function(app) {
 
   app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
     next();
   });
 };
