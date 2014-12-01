95eca827455c12e5f50cfce11cb0ff7ead9463a3
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-01 16:05:38 -0300

    add response headers

--- config/headers.js
+++ config/headers.js
@@ -8,6 +8,7 @@ module.exports = function(app) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Email-Needs-Validation,X-Quota-Per-Item,X-Quota-Items-Limit,X-RateLimit-Limit,X-RateLimit-Remaining');
     next();
   });
 };
