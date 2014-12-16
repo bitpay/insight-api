0ca28cd5c606651a700a78bfed0a82ea95a80323
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-16 12:25:01 -0300

    update error message

--- plugins/emailstore.js
+++ plugins/emailstore.js
@@ -34,7 +34,7 @@
     },
     NOT_FOUND: {
       code: 404,
-      message: 'Credentials were not found'
+      message: 'Email already confirmed or credentials were not found '
     },
     INTERNAL_ERROR: {
       code: 500,
