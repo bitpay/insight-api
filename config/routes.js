663703ab564c54a75ed54ebf1eec48eb94473c87
Matias Alejo Garcia <ematiu@gmail.com>
2015-07-05 10:53:00 -0300

    rm unused plugins

--- config/routes.js
+++ config/routes.js
@@ -60,20 +60,8 @@ module.exports = function(app) {
   // Email store plugin
   if (config.enableEmailstore) {
     var emailPlugin = require('../plugins/emailstore');
-    app.post(apiPrefix + '/email/save', emailPlugin.save);
     app.get(apiPrefix + '/email/retrieve', emailPlugin.retrieve);
-    app.post(apiPrefix + '/email/change_passphrase', emailPlugin.changePassphrase);
-
-    app.post(apiPrefix + '/email/validate', emailPlugin.validate);
-    app.get(apiPrefix + '/email/validate', emailPlugin.validate);
-
-    app.post(apiPrefix + '/email/register', emailPlugin.oldSave);
     app.get(apiPrefix + '/email/retrieve/:email', emailPlugin.oldRetrieve);
-
-    app.post(apiPrefix + '/email/delete/profile', emailPlugin.eraseProfile);
-    app.get(apiPrefix + '/email/delete/item', emailPlugin.erase);
-
-    app.get(apiPrefix + '/email/resend_email', emailPlugin.resendEmail);
   }
 
   // Currency rates plugin
