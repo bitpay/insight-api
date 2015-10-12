7c5ed99494a0816aee3877710ec8484d5c32a40a
Riccardo Casatta <riccardo.casatta@gmail.com>
2015-10-12 18:27:54 +0200

    Adding height of transaction to returned data

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -64,8 +64,20 @@ exports.transaction = function(req, res, next, txid) {
       if (err)
         return common.handleErrors(err, res);
 
-      req.transaction = tx.info;
-      return next();
+      req.transaction = tx.info;   
+      console.log("getHeight(" + tx.info.blockhash );
+      if(tx.info.blockhash) {
+        bdb.getHeight(tx.info.blockhash , function(err, height) {
+          if (err) return cb(err);
+          console.log("height="+height);
+          tx.info.height = height;
+
+          return next();
+
+        });
+      } else {
+        return next();
+      }
     });
 
   });
