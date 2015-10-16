3dfc1bc9e34c6bdc159e6945582997921f411cfa
Braydon Fuller <braydon@bitpay.com>
2015-10-16 13:34:42 -0400

    Switch to use bitcore-lib.

--- test/transactions.js
+++ test/transactions.js
@@ -1,7 +1,7 @@
 'use strict';
 var should = require('should');
 var sinon = require('sinon');
-var bitcore = require('bitcore');
+var bitcore = require('bitcore-lib');
 var TxController = require('../lib/transactions');
 var _ = require('lodash');
 
