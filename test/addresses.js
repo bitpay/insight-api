3390e7b5db9ddded7c817194606a1169606484e4
ajv <voisine@gmail.com>
2016-05-26 15:14:02 -0700

    change amountSat to satoshis to match insight.bitpay.com/api

--- test/addresses.js
+++ test/addresses.js
@@ -283,8 +283,8 @@ describe('Addresses', function() {
               "vout": 1,
               "ts": 1441116143,
               "scriptPubKey": "76a914d2ec20bb8e5f25a52f730384b803d95683250e0b88ac",
-              "amountSat": 53320000,
               "amount": 0.5332,
+              "satoshis": 53320000,
               "confirmations": 50,
               "confirmationsFromCache": true
           }
@@ -327,8 +327,8 @@ describe('Addresses', function() {
               "vout": 1,
               "ts": 1441116143,
               "scriptPubKey": "76a914d2ec20bb8e5f25a52f730384b803d95683250e0b88ac",
-              "amountSat": 53320000,
               "amount": 0.5332,
+              "satoshis": 53320000,
               "confirmations": 50,
               "confirmationsFromCache": true
           },
@@ -338,8 +338,8 @@ describe('Addresses', function() {
               "vout": 2,
               "ts": 1441116143,
               "scriptPubKey": "76a914583df9fa56ad961051e00ca93e68dfaf1eab9ec588ac",
-              "amountSat": 289829,
               "amount": 0.00289829,
+              "satoshis": 289829,
               "confirmations": 50,
               "confirmationsFromCache": true
           }
