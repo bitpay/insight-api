f0970b734c6d89221ab2b77379fbf2211ef11e37
digibyte <dev@digibyte.co>
2016-06-13 18:44:44 +0800

    Comma Display Algo

--- app/controllers/blocks.js
+++ app/controllers/blocks.js
@@ -144,7 +144,7 @@ exports.list = function(req, res) {
               hash: b.hash,
               time: b.ts || info.time,
               txlength: info.tx.length,
-              poolInfo: info.poolInfo
+              poolInfo: info.poolInfo,
               algo: info.pow_algo
             });
           });
